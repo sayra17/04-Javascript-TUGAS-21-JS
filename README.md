d1866d91352c193dbbccfd677e1b16e6ed3be524
sayra17 <agussayra03@gmail.com>
2022-02-05 17:14:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-21-JS
---
+++ tugas21.js
@@ -0,0 +1,6 @@
+function ganti() {
+    var kata = "saya belajar masak nasi goreng"
+    console.log(kata)
+    console.log(kata.replace("masak", "makan"))
+}
+ganti()
